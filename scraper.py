8ae173b27434c9dd993205fbc73c6fd3255a12d3
Einar Sandberg <einars92@gmail.com>
2020-02-28 13:27:47 +0100

    now possible to get matches that hasn't been played yet

--- scraper.py
+++ scraper.py
@@ -6,7 +6,7 @@ import os
 def scrape():
     # Url fetched by https://www.svenskfotboll.se/serier-cuper/spelprogram/allsvenskan-herrar/77486/ when clicking "Omgång"
     # Contains json of format {data: html_content}
-    rounds_url = 'https://www.svenskfotboll.se/api/sort-games/?contentID=5701&ftid=77486&sortBy=round'
+    rounds_url = 'https://www.svenskfotboll.se/api/sort-games/?contentID=5701&ftid=82492&sortBy=round'
     req = Request(rounds_url, headers={'User-Agent': 'Mozilla/5.0'})
     resp = urlopen(req).read()
 
@@ -24,15 +24,21 @@ def scrape():
 
 def create_matches(teams_el_list: list, goals_el_list: list):
     matches = []
+    
     for i in range(1, len(teams_el_list), 2):
-        matches.append(
-            create_match(
+        if len(goals_el_list) > 0:
+            match = create_match(
                 home_team=teams_el_list[i-1].getText(),
                 away_team=teams_el_list[i].getText(),
                 home_goals=goals_el_list[i-1].getText(),
                 away_goals=goals_el_list[i].getText(),
             )
-        )
+        else:
+            match = create_match_without_goals(
+                home_team=teams_el_list[i-1].getText(),
+                away_team=teams_el_list[i].getText(),
+            )
+        matches.append(match)
     
     return matches
 
@@ -42,6 +48,12 @@ def create_match(home_team: str, away_team: str, home_goals: str, away_goals: st
         'awayTeam': away_team,
         'homeGoals': home_goals,
         'awayGoals': away_goals,
+    }
+
+def create_match_without_goals(home_team: str, away_team: str):
+    return {
+        'homeTeam': home_team,
+        'awayTeam': away_team,
     }
 
 def write_json(matches: list):
